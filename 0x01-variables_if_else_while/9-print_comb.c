9b22198b7917a3eeb8379279f024964e9934dd1c
culoves <lovecynthiau@gmail.com>
2022-03-11 14:53:04 -0800

    1

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers
+ * numbers are separated with a comma.
+ *
+ * Return - Always 0 (success)
+ */
+
+int main(void)
+{
+int n;
+
+for (n = 48; n < 58; n++)
+{
+putchar(n);
+if (n != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+
+return (0);
+}
